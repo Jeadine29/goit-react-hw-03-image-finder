865fd3169ae24485cb4a3de150ca20f085526835
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-06-19 12:14:39 +0800

    update components and styles

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const ImageGalleryItem = ({ webformatURL, largeImageURL, onClick }) => {
+  return (
+    <li className="gallery-item" onClick={onClick}>
+      <img src={webformatURL} alt="" />
+    </li>
+  );
+};
+
+export default ImageGalleryItem;
